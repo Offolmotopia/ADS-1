fea86a939e152146a1cabbe575816f573f96398a
Offolmotopia <74813567+Offolmotopia@users.noreply.github.com>
2021-03-23 17:00:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,19 +6,19 @@ int end = size - 1;
 int count = 0;
 
 while (start < end) {
-int k = (start + end) / 2;
-if (arr[k] < value) {
-start = k+1;
-} else {
-end = k;
-}
+  int k = (start + end) / 2;
+  if (arr[k] < value) {
+    start = k+1;
+  } else {
+    end = k;
+  }
 }
 
 if (arr[start] == value) {
-while (arr[start] == value) {
-count++;
-start++;
-}
+  while (arr[start] == value) {
+    count++;
+    start++;
+  }
 }
 
 return count;
