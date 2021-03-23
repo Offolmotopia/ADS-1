13e79996717212672b275e2192225aeff912948d
Offolmotopia <74813567+Offolmotopia@users.noreply.github.com>
2021-03-23 21:12:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,1 +1,26 @@
-
+// Copyright 2021 NNTU-CS
+int cbinsearch(int *arr, int size, int value) {
+    int R, L = 0, sum = 0, midl;
+    R = size - 1;
+    while (L < R) {
+        midl = (R - L) / 2 + L;
+        if (arr[midl] == value) {
+            while (arr[midl - 1] == value) {
+                sum++;
+                midl--;
+            }
+            midl += sum;
+            while (arr[midl] == value) {
+                midl++;
+                sum++;
+            }
+            return sum;
+        }
+        if (arr[midl] > value) {
+            R = midl;
+        } else {
+            L = midl + 1;
+        }
+    }
+    return 0;
+}
